92a9f4169670ecad4f6a881ef627de803b620d15
Daniele-Biscari <biscari.daniele@gmail.com>
2024-05-14 18:21:06 +0200

    modifica stile

--- meteo-si/src/pages/HomePage.jsx
+++ meteo-si/src/pages/HomePage.jsx
@@ -6,20 +6,20 @@ const HomePage = () => {
     <div className="container-fluid">
       <h1 className="text-center mb-4">Come funziona questa app</h1>
       <div className="d-flex flex-column justify-content-center align-items-center gap-4 align-items-center ">
-        <div className="d-flex flex-wrap justify-content-center">
+        <div className="d-flex flex-wrap justify-content-evenly ">
           <p className="pCustom">
             Andando nella sezione meteo è possibile inserire la latitudine e la
             longitudine della tua città per poter ottenere i dati metereologici
           </p>
-          <img src={image1} alt="sole con nuovola e pioggia" width={400} />
+          <img className="imageSize" src={image1} alt="sole con nuovola e pioggia" width={400} />
         </div>
 
-        <div className="d-flex flex-wrap justify-content-center">
+        <div className="d-flex flex-wrap justify-content-evenly w-100 ">
           <p className="pCustom">I dati vengono aggiornati in tempo reale</p>
           <i className="fontCustom bi bi-clock"></i>
         </div>
 
-        <div className="d-flex flex-wrap justify-content-center">
+        <div className="d-flex flex-wrap justify-content-evenly w-100 ">
           <p className="pCustom">salva i tuoi dati dentro il Database</p>
           <i className="fontCustom bi bi-database"></i>
         </div>
